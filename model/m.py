354ca6f6d831331b699bdf18103eeb19bd540d9f
makinyilmaz <77252309+makinyilmaz@users.noreply.github.com>
2022-02-21 14:07:13 +0300

    Update m.py

--- model/m.py
+++ model/m.py
@@ -1,5 +1,4 @@
 
-# coding: utf-8
 import os
 
 import copy
@@ -14,7 +13,7 @@ import numpy as np
 from .layers import MVCompressor, ResidualCompressor, Mask
 from .flow import Network
 
-device = torch.device("cuda:0")
+device = torch.device("cuda")
 
 
 class Model(nn.Module):
@@ -28,6 +27,8 @@ class Model(nn.Module):
         self.mv_compressor = MVCompressor()
         self.residual_compressor = ResidualCompressor()
         self.masknet = Mask()
+
+        self.upsample_flow = nn.Upsample(scale_factor=4, mode='bilinear')
     
     def forward(self, x_before, x_current, x_after, train):
         
@@ -35,34 +36,50 @@ class Model(nn.Module):
         N, C, H, W = x_current.size()
         num_pixels = N * H * W
         
-        flow_ba = self.estimate_flow(x_after, x_before) / 2.
-        flow_ab = self.estimate_flow(x_before, x_after) / 2.
+        flow_ba = F.avg_pool2d(self.FlowNet(x_before, x_after) / 2., 4)
+        flow_ab = F.avg_pool2d(self.FlowNet(x_after, x_before) / 2., 4)
+
+        nn,cc,hh,ww = flow_ab.size()
+
+        flow_ba = self.pad(flow_ba)
+        flow_ab = self.pad(flow_ab)
+
+        flow_cb = F.avg_pool2d(self.FlowNet(x_current, x_before), 4)
+        flow_ca = F.avg_pool2d(self.FlowNet(x_current, x_after), 4)
+
+        flow_cb = self.pad(flow_cb)
+        flow_ca = self.pad(flow_ca)
         
-        flow_bc = self.estimate_flow(x_current, x_before)
-        flow_ac = self.estimate_flow(x_current, x_after)
-        
-        diff_flow = torch.cat([(flow_bc - flow_ba), (flow_ac - flow_ab)], dim=1)
+        diff_flow = torch.cat([flow_cb - flow_ab, flow_ca - flow_ba], dim=1)
         flow_result = self.mv_compressor(diff_flow)
         
-        flow_bc_hat, flow_ac_hat = torch.chunk(flow_result["x_hat"] + torch.cat([flow_ba, flow_ab], dim=1), 2, dim=1)
+        flow_cb_hat, flow_ca_hat = torch.chunk(flow_result["x_hat"], 2, dim=1)
+        flow_cb_hat = flow_cb_hat + flow_ab
+        flow_cb_hat = self.upsample_flow(flow_cb_hat[:, :, :hh, :ww])
+        flow_ca_hat = flow_ca_hat + flow_ba
+        flow_ca_hat = self.upsample_flow(flow_ca_hat[:, :, :hh, :ww])
         
-        fw, bw = self.backwarp(x_before, flow_bc_hat), self.backwarp(x_after, flow_ac_hat)
+        fw, bw = self.backwarp(x_before, flow_cb_hat), self.backwarp(x_after, flow_ca_hat)
         
         mask = self.masknet(torch.cat([fw, bw], dim=1)).repeat([1, 3, 1, 1])
         
         x_current_hat = mask*fw + (1.0 - mask)*bw
-        x_current_hat_avg = (fw + bw)/2.
         
         residual = x_current - x_current_hat
         residual_result = self.residual_compressor(residual)
         residual_hat = residual_result["x_hat"]
         
-        residual_avg = x_current - x_current_hat_avg
-        residual_result_avg = self.residual_compressor(residual_avg)
-        residual_hat_avg = residual_result_avg["x_hat"]
+        x_current_hat = residual_hat + x_current_hat
         
-        x_current_final = residual_hat + x_current_hat
-        x_current_final_avg = residual_hat_avg + x_current_hat_avg
+        rate_flow = sum(
+            (torch.log(likelihoods).sum() / (-math.log(2) * num_pixels))
+            for likelihoods in flow_result["likelihoods"].values()
+        )
+        
+        rate_residual = sum(
+            (torch.log(likelihoods).sum() / (-math.log(2) * num_pixels))
+            for likelihoods in residual_result["likelihoods"].values()
+        )
         
         size_flow = sum(
             (torch.log(likelihoods).sum() / (-math.log(2)))
@@ -74,42 +91,37 @@ class Model(nn.Module):
             for likelihoods in residual_result["likelihoods"].values()
         )
         
-        size_residual_avg = sum(
-            (torch.log(likelihoods).sum() / (-math.log(2)))
-            for likelihoods in residual_result_avg["likelihoods"].values()
-        )
-        
                 
         
-
-        return x_current_final, x_current_final_avg, size_flow.item() + size_residual.item(), size_flow.item() + size_residual_avg.item(), flow_bc, flow_ac, flow_bc_hat, flow_ac_hat, mask, fw, bw, x_current_hat, x_current_hat_avg
+        if train:
+            return x_current_hat, (rate_flow + rate_residual)/2.
+        else:
+            return x_current_hat, (rate_flow + rate_residual)/2., size_flow.item() + size_residual.item()
         
     
-    def estimate_flow(self, tenFirst, tenSecond):
-        
-        h = tenFirst.size()[2]
-        w = tenFirst.size()[3]
-        h_p = int(math.floor(math.ceil(h / 32.0) * 32.0))
-        w_p = int(math.floor(math.ceil(w / 32.0) * 32.0))
-        
+    def pad(self, im):
+        (m,c,w,h) = im.size()
 
-        tenPreprocessedFirst = torch.nn.functional.interpolate(input=tenFirst, size=(h_p, w_p), mode='bilinear', align_corners=False)
-        tenPreprocessedSecond = torch.nn.functional.interpolate(input=tenSecond, size=(h_p, w_p), mode='bilinear', align_corners=False)
+        p1 = (64 - (w % 64)) % 64
+        p2 = (64 - (h % 64)) % 64
 
-        tenFlow = torch.nn.functional.interpolate(input=self.FlowNet(tenPreprocessedFirst, tenPreprocessedSecond), size=(h, w), mode='bilinear', align_corners=False)
-        tenFlow[:, 0, :, :] *= float(w) / float(w_p)
-        tenFlow[:, 1, :, :] *= float(h) / float(h_p)
-
-        return tenFlow
+        pad = nn.ReflectionPad2d(padding=(0, p2, 0, p1))
+        return pad(im)
     
-    
+
     def backwarp(self, tenInput, tenFlow):
-        tenHor = torch.linspace(-1.0 + (1.0 / tenFlow.shape[3]), 1.0 - (1.0 / tenFlow.shape[3]), tenFlow.shape[3]).view(1, 1, 1, -1).expand(-1, -1, tenFlow.shape[2], -1)
-        tenVer = torch.linspace(-1.0 + (1.0 / tenFlow.shape[2]), 1.0 - (1.0 / tenFlow.shape[2]), tenFlow.shape[2]).view(1, 1, -1, 1).expand(-1, -1, -1, tenFlow.shape[3])
 
-        backwarp_tenGrid = torch.cat([ tenHor, tenVer ], 1).to(device).float()
+        tenHor = torch.linspace(-1.0 + (1.0 / tenFlow.shape[3]), 1.0 - (1.0 / tenFlow.shape[3]),
+                            tenFlow.shape[3]).view(1, 1, 1, -1).expand(-1, -1, tenFlow.shape[2], -1)
+        tenVer = torch.linspace(-1.0 + (1.0 / tenFlow.shape[2]), 1.0 - (1.0 / tenFlow.shape[2]),
+                            tenFlow.shape[2]).view(1, 1, -1, 1).expand(-1, -1, -1, tenFlow.shape[3])
+
+        backwarp_tenGrid = torch.cat([ tenHor, tenVer ], 1).to(device)
         # end
 
-        tenFlow = torch.cat([ tenFlow[:, 0:1, :, :] / ((tenInput.shape[3] - 1.0) / 2.0), tenFlow[:, 1:2, :, :] / ((tenInput.shape[2] - 1.0) / 2.0) ], 1)
+        tenFlow = torch.cat([ tenFlow[:, 0:1, :, :] / ((tenInput.shape[3] - 1.0) / 2.0),
+                          tenFlow[:, 1:2, :, :] / ((tenInput.shape[2] - 1.0) / 2.0) ], 1)
 
-        return torch.nn.functional.grid_sample(input=tenInput, grid=(backwarp_tenGrid + tenFlow).permute(0, 2, 3, 1), mode='bilinear', padding_mode='border', align_corners=False)
+        return torch.nn.functional.grid_sample(input=tenInput,
+                                           grid=(backwarp_tenGrid + tenFlow).permute(0, 2, 3, 1),
+                                           mode='bilinear', padding_mode='border', align_corners=False)
